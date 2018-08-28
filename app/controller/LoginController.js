fee22259f168f9b6c219c6daaad839a21ecb5c9e
周立 <li.zhou@huilianyi.com>
2018-08-28 17:47:54 +0800

    添加注释，koa-seesion

--- app/controller/LoginController.js
+++ app/controller/LoginController.js
@@ -9,6 +9,10 @@ async function loginController(ctx) {
     console.log(res);
     ctx.body = res;
     if (ctx.body.status === 0) {
+        // koa-session
+        // ctx.session.userinfo = ctx.body.data.userId;
+        // console.log(ctx.session.userinfo);
+        // 退出时 ctx.session = null
         ctx.cookies.set('userId', `${ctx.body.data.userId}`, {
             maxAge: 7 * 24 * 3600 * 1000
         });
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
 const Router = require('koa-router');
-
+// const session = require('koa-generic-session');
 const config = require('./app/config/config.dev');
 const router = require('./app/route/router');
 const indexController = require('./app/controller/index');
@@ -15,6 +15,22 @@ const indexController = require('./app/controller/index');
 const routerForAllow = new Router();
 const port = process.env.PORT || config.port;
 const app = new Koa();
+
+//配置session的中间件
+/*cookie的签名
+app.keys = ['json119'];
+const sessionConfig = {
+    key: 'koa:sess',
+    maxAge: 86400000,
+    overwrite: true,
+    httpOnly: true,
+    signed: true,
+    rolling: true,
+    renew: false,
+};
+app.use(session(sessionConfig, app));
+*/
+
 //日志处理
 app.use(logger());
 //请求体处理
